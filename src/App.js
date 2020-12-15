49f4f1b9d3216996f596ef4309c275cdd155e2f8
frankcollins3 <fwc3rd@gmail.com>
2020-12-14 19:42:28 -0500

    [react-todos] add the to-do exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import ListItem from './Components/ListItem';
+
+let listItems = ["wake up", "shake up", "code code code", "what is life", "sleep"];
+
+let listAllItems = listItems.map((listeItem, idx) => {
+  return <ListItem doThis={listItem} key={idx}/>
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>To-do List</header>
+      <div>
+        <ul>
+          <>{listAllItems}</>
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Components/ListItem.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ListItems = (props) => {
+    return (
+        <li>{props.doThis}</li>
+    )
+}
+export default ListItems
